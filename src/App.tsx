25fe15f7ea24db95f72165fd65302623b8081817
Arefevpro <arefevmsk@gmail.com>
2024-05-12 13:17:05 +0300

    ./

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
 import "./App.css";
-import { Todolist } from "./Todolist";
+import { TodoLIst } from "./Todolist";
 import React, { useState } from "react";
 import styled from "styled-components";
 
-export type TaskType = {
+export type BooksType = {
   id: number
-  title: string
+  name: string
   isDone: boolean
-}
-
-
-function App() {
-  const tasks1: TaskType[] = [
-    { id: 1, title: "HTML&CSS", isDone: true },
-    { id: 2, title: "JS", isDone: true },
-    { id: 3, title: "ReactJS", isDone: false },
-    { id: 4, title: 'Redux', isDone: false },
+  }
+  
+  const booksListOne: BooksType[] = [
+    {id: 1, name: "Судные дни", isDone: false},
+    {id: 1, name: "Дракула", isDone: true},
+    {id: 1, name: "Ритуал", isDone: true},
+    {id: 1, name: "Чужой", isDone: false},
   ];
-
-  const tasks2: TaskType[] = [
-    { id: 1, title: "Hello world", isDone: true },
-    { id: 2, title: "I am Happy", isDone: false },
-    { id: 3, title: "Yo", isDone: false },
-    { id: 4, title: 'Redux', isDone: false },
-  ];
-  return (
-    <Wrapper>
-      <Todolist title="Songs" tasks={tasks1} date={"13.13.1666"}/>
-      <Todolist title="Books" tasks={tasks2} />
-    </Wrapper>
-  );
-}
-
-export default App;
-
-const Wrapper = styled.div`
-  display: flex;
-  gap: 30px;
-  margin: 30px;
-`;
+  
+  
+  const booksListTwo: BooksType[] = [
+    {id: 1, name: "Странник", isDone: false},
+    {id: 1, name: "Питер", isDone: true},
+    {id: 1, name: "Север", isDone: true},
+    {id: 1, name: "Крым", isDone: true},
+  ] 
+  
+  function App() {
+    return (
+      <AppWrapper>
+      <TodoLIst title="Ужасы" booksList={booksListOne}/>
+      <TodoLIst title="Метро 2033" booksList={booksListTwo}/>
+      </AppWrapper>
+    );
+  }
+  
+  export default App;
+  
+  
+  const AppWrapper = styled.div`
+    display: flex;  
+    gap: 50px;
+  `
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,47 +1,59 @@
-import styled from "styled-components";
-import { TaskType } from "./App"
+import styled from 'styled-components'
+import { BooksType } from "./App"
 
 type PropsType = {
   title: string
-  tasks: TaskType[]
-  date?: string
+  booksList: BooksType[]
 }
 
-
-
-export const Todolist = ({title, tasks, date}: PropsType) => {
+export const TodoLIst = ({title, booksList}: PropsType) => {
   return (
-    <TodolistWrapper>
-      <h3>{title}</h3>
-      <div>
-        <input />
-        <button>+</button>
-      </div>
-      <ul>
-        {tasks.map(task => {
-          return (
-            <li>
-              <input type="checkbox" checked={task.isDone} />
-              <span>{task.title}</span>
-            </li>
-          )
-        })}
-      </ul>
+    <BooksWrapper>
+      <Title>{title}</Title>
+      <ListWrapper>
+       {booksList.map((books) => {
+        debugger
+        return (
+          <li>
+            <input type="checkbox" checked={books.isDone}/>
+            <span>{books.name}</span>
+          </li>
+        )
+       }
+      )}
+      </ListWrapper>
       <div>
         <button>All</button>
         <button>Active</button>
         <button>Completed</button>
-        <div>{date}</div>
       </div>
-    </TodolistWrapper>
-  );
+    </BooksWrapper>
+  );  
 };
 
-export default Todolist;
 
-const TodolistWrapper = styled.div`
-  padding: 20px;
-  background-color: #08b140;
-  max-width: 200px;
-  height: 250px;
-`;
+const BooksWrapper = styled.div`
+padding: 20px;
+background-color: #10b2c0;
+border: 3px solid black;
+color: black;
+
+&:hover {
+  outline: 3px solid red;
+}
+
+&:last-child {
+  background-color: #0bd15a;
+}
+`
+
+const ListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  list-style-type: none; 
+  margin-bottom: 10px;
+` 
+const Title = styled.h3`
+    
+`
