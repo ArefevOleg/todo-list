d3618843abffec5abe6d2b8140b14c943d5bc1ed
Arefevpro <arefevmsk@gmail.com>
2025-01-21 22:24:09 +0300

    ./main

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,10 @@
-import React, {useState} from 'react';
-import TodoList from "./TodoList";
-import { v1 } from 'uuid'
-
-
-export type TaskType = {
-    id: string
-    title: string
-    isDone: boolean
-}
+import React from 'react';
 
 const App = () => {
-    let [tasks, setTasks] = useState<TaskType[]>(
-        [
-            {id: v1(), title: 'HTML&CSS', isDone: true},
-            {id: v1(), title: 'JS', isDone: true},
-            {id: v1(), title: 'ReactJS', isDone: false},
-            {id: v1(), title: 'Redux', isDone: false},
-        ]
-    )
-
-const removeTask = (taskId: string) => {
-    const filteredTasks = tasks.filter(task => task.id !== taskId)
-    setTasks(filteredTasks)
-}
-
-
     return (
-        <>
-            <TodoList
-                removeTask={removeTask}
-                tasks={tasks}
-                title="What to learn"
-            />
-        </>
+        <div>
+            
+        </div>
     );
 };
 
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,43 +1,9 @@
 import React from 'react';
-import {TaskType} from "./App";
-import styles from "./styles/TodoList.module.css"
-import {Button} from "./components/Button";
 
-type PropsType = {
-    tasks: TaskType[]
-    title: string
-    removeTask: (taskId: string) => void
-}
-
-
-const TodoList = ({tasks, title, removeTask}: PropsType) => {
+const TodoList = () => {
     return (
-        <div className={styles.mainWrapper}>
-            <h3>{title}</h3>
-            <div>
-                <input type="text"/>
-                <Button onClick={() => ("+")} title="+" />
-            </div>
-            {tasks.length === 0 ? (
-                <span>no tasks</span>
-            ) : (
-                <ul className={styles.listItem}>
-                    {tasks.map(task => {
-                        return (
-                            <li key={task.id} className={styles.item}>
-                                <input type="checkbox" checked={task.isDone}/>
-                                <span>{task.title}</span>
-                                <Button onClick={() => removeTask(task.id)} title="x" />
-                            </li>
-                        )
-                    })}
-                </ul>
-            )}
-            <div className={styles.btnWrapper}>
-                <Button onClick={() => ("all")} title="all" />
-                <Button onClick={() => ("active")} title="active" />
-                <Button onClick={() => ("completed")} title="completed" />
-            </div>
+        <div>
+            
         </div>
     );
 };
